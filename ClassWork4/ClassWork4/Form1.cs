e481823bb8608180cb1d03e13e13909e4ce2976b
GhostMan11 <ghost.man4477@gmail.com>
2019-04-03 13:20:52 +0400

    ClassWork4
    delegats

---
+++ ClassWork4/ClassWork4/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassWork4
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            mydelegate nc;
+            mydelegate nc1 = new mydelegate(core.add);
+            mydelegate nc2 = new mydelegate(core.shouting);
+            mydelegate nc3 = new mydelegate(core.multiplication);
+            mydelegate nc4 = new mydelegate(core.divide);
+
+
+            nc = nc1;
+            nc += nc3;
+            nc += nc2;
+            nc(2);
+
+            label2.Text = core.num.ToString();
+
+        }
+    }
+}
---
+++ ClassWork4/ClassWork4/core.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork4
+{
+    delegate int mydelegate(int os);
+
+    class core
+    {
+        public static int num = 10;
+
+        public static int add(int n1)
+        {
+            num += n1;
+            return num;
+        }
+        public static int shouting(int n2)
+        {
+            num -= n2;
+            return num;
+        }
+
+        public static int multiplication(int n3)
+        {
+            num *= n3;
+            return num;
+        }
+
+        public static int divide(int n4)
+        {
+            num /= n4;
+            return num;
+        }
+
+    }
+}
